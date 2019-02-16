30ab347a1aa4a5b2dab740b562c73a8482d7d801
Michael Wepf <mwepf1@gmail.com>
2019-02-16 11:56:56 +0100

    Added php and png files

---
+++ php/database_kitob.php
@@ -0,0 +1,18 @@
+<?php 
+/*$host = "10.35.47.161";
+$username = "k60511_kitob";
+$password = '$pw$123$';
+$dbname = "k60511_kitob";*/
+$host = "mwepf1gm.mysql.db.hostpoint.ch";
+$username = "mwepf1gm_kitob";
+$password = 'Welcome$kitob.1';
+$dbname = "mwepf1gm_kitob";
+
+/* SQL connect */
+/*-----------------open when use ajaxphp 
+$conn = new mysqli($host, $username, $password, $dbname);
+if($conn->connect_error) {
+	$error = $conn->connect_error;
+    die("Connection to database failed: " . $conn->connect_error);
+}--------------------*/
+?>
