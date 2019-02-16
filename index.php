2668565abb1131630b5feebb3d0f78a0a2b10c94
Michael Wepf <mwepf1@gmail.com>
2019-02-16 12:03:19 +0100

    Added php and png files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Китоб</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+	<img src="https://a2fa0.webhosting.systems/smb/file-manager/show-image?currentDir=%2Fkitob&file=logo.png" alt="logo" width="40%"/>
+    <h1>In development</h1>
+    <a href="test.php">to see sth click here</a>
+</body>
+</html>
