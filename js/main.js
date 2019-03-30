234a2ae4805b39640630deab33f93cf7d89b925d
Michael Wepf <mwepf1@gmail.com>
2019-03-30 19:03:53 +0100

    Display text in page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // JS loaded as last doc, so no need to wait for onload
 
 /* Read URL-Reques and write to console */
-path = window.location.pathname; // get requested path
-requestedPath = decodeURI(path).substr(1, ); // decode kyrillic and omit slash
-console.log(requestedPath); // show in console
-document.getElementById("path").innerHTML = requestedPath; // and in html too
+function reloadText() {
+    path = window.location.pathname; // get requested path
+    requestedPath = decodeURI(path).substr(1); // decode kyrillic and omit slash
+    console.log(requestedPath); // DEV-Info
+    document.getElementById("path").innerHTML = requestedPath; // and in html too
+    getText(requestedPath);
+}
 
 /* Request text from server */
 function getText(book, chapter = 1, verses = 1) {
@@ -13,7 +16,7 @@ function getText(book, chapter = 1, verses = 1) {
     var request = {
         book: book,
         chapter: chapter,
-        verses: verses,
+        verses: verses
     };
     requestString = JSON.stringify(request);
 
@@ -23,19 +26,35 @@ function getText(book, chapter = 1, verses = 1) {
         url: "/php/getText.php", // standard processing file
         //contentType: "application/json; charset=utf-8",     // set contentType
         //breaks all: dataType: "json",                                   // use only json
-        data: "data=" + requestString, // send request to server
+        data: "data=" + requestString // send request to server
     }).done(function (data) {
-        //console.log(data); // DEV
+        //console.log(data);            // DEV-Info
         renderText(data);
     });
 }
 
 /* Renders json-text to html */
-function renderText(receivedText){
+function renderText(receivedText) {
+    // Define vars for have them available in whole function not only in sub'
+    // first and lastVerse are used for printing out them if needed
+    var book, chapter, firstVerse = false,
+        lastVerse = false,
+        text = "";
     var jsonText = $.parseJSON(receivedText);
-    console.log(jsonText);
+    console.log(jsonText); // DEV-Info
+
+    // Iterate over json
+    $.each(jsonText, function (key, value) {
+        book = value['book'];
+        chapter = value['chapter'];
+        firstVerse ? lastVerse = value['verse'] : firstVerse = value['verse'];
+        text = text + " <b>" + value['verse'] + "</b> " + value['text'];
+    });
+    lastVerse ? verseNumbers = firstVerse + "-" + lastVerse : verseNumbers = firstVerse;
+    $('h2.chapter').html(book + " " + chapter + ":" + verseNumbers);
+    $('div.text').html(text);
 }
 
 /* Execute now */
 // TODO: don't request directly, first split apart book, chapter, ...
-getText(requestedPath);
+reloadText();
