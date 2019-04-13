2cec91a4d0bb74b4ca9c10e20cd1ccb3210d50f4
Michael Wepf <mwepf1@gmail.com>
2019-04-13 23:24:00 +0200

    Added bootstrap navbar

--- js/main.js
+++ js/main.js
@@ -270,3 +270,12 @@ function scrollToVerse() {
     }
 }
 
+// show the keyboard users currently selected key
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // the "I am a keyboard user" key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+window.addEventListener('keydown', handleFirstTab);
+
