8f1bad74e132eda4287b7b2e32eecf275b7273ca
Michael Wepf <mwepf1@gmail.com>
2019-05-09 11:33:51 +0200

    Added book color

--- php/getChapters.php
+++ php/getChapters.php
@@ -55,7 +55,8 @@ function giveChapters($con) {
     $sql = "SELECT 
                 b.book_number AS bookNumber,
                 b.short_name AS shortBook,
-                b.long_name AS longBook, 
+                b.long_name AS longBook,
+                b.book_color AS color,
 		        COUNT( DISTINCT v.chapter) AS chapterCount
 	        FROM books as b
 	        JOIN verses AS v ON b.book_number = v.book_number
