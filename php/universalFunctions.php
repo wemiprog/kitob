2d8d90a4535dc11d28ffb90941e0bdf703ffe8c4
Michael Wepf <mwepf1@gmail.com>
2019-05-14 18:18:46 +0200

    Added km92 declaration

--- php/universalFunctions.php
+++ php/universalFunctions.php
@@ -3,7 +3,7 @@ function createDBCon($translation)
 {
     // Translation declarations
     $cfg_kmn = $GLOBALS['configdir'] . 'database_kitob.php';
-    //TODO: Add more translation declarations
+    $cfg_km92 = $GLOBALS['configdir'] . 'db_kitob_km92.php';
 
     switch ($translation) {
         case "kmn":
@@ -12,6 +12,11 @@ function createDBCon($translation)
         case "кмн":
             $cfg_current = $cfg_kmn;
             break;
+        case "km92":
+            $cfg_current = $cfg_km92;
+            break;
+        case "км92":
+            $cfg_current = $cfg_km92;
         default:
             $cfg_current = $cfg_kmn;
     }
