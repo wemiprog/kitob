da14b86ea9c6a36a92cb4f99b26889f7c30d916a
Michael Wepf <mwepf1@gmail.com>
2019-08-07 14:04:19 +0200

    Moved userConfig to own dir

--- php/getDefaultConfig.php
+++ php/getDefaultConfig.php
@@ -8,9 +8,20 @@ function setVar($array, $var, $value)
     }
     return $array;
 }
+function create($array)
+{
+    if (isset($array)){
+        // all ok
+        return $array;
+    } else {
+        $array = [];
+        return $array;
+    }
+}
 if($script) {
     // Default html and php definitions
     //$html = setVar($html, "lang", "newvalue");
+    $html = create($html);
     $html = setVar($html, "lang", "en");
     $html = setVar($html, "title", "KITOB | The Bible");
     $html = setVar($html, "trans1", "ASV");
@@ -18,25 +29,27 @@ if($script) {
     $html = setVar($html, "choose", "Choose");
     $html = setVar($html, "loading", "Chosen text is loading ...");
 
+    $vars = create($vars);
     $vars = setVar($vars, "homedir", $homedir);
     $vars = setVar($vars, "configDirFromHome", "/config/");
     $vars = setVar($vars, "configDir", $configdir);
     $vars = setVar($vars, "dataDir", $datadir);
     $vars = setVar($vars, "defBook", "470");
     $vars = setVar($vars, "defTranslation", "asv");
-    $vars = setVar($vars, "allowedChars", "QWERTZUIOPASDFGHJKLYXCVBNMqwertzuioplkjhgfdsayxcvbnm:,.-1234567890");
+    $vars = setVar($vars, "allowedChars", "ёйқукенгшҳзхъӯғэждлорпавҷфячсмитӣбюЁҒӮЪХЗҲШГНЕКУҚЙФҶВАПРОЛДЖЭЮБӢТИМСЧЯQWERTZUIOPASDFGHJKLYXCVBNMqwertzuioplkjhgfdsayxcvbnm:,.-1234567890");
     $vars = setVar($vars, "replaceChars", ""); // odd = target letter, even = source letter
-    $t1 = ["name" => "asv","file" => "$configdir/db_kitob_asv.php","alias" => "asv"];
+    $t1 = ["name" => "asv","file" => "$configdir/database_kitob.php","alias" => "asv"];
     $vars = setVar($vars, "t1", $t1);
 } else { ?>
 console.log("defaults loaded");
+if (typeof df == 'undefined') { df = {};}
 function setVar(name, value) {
     if(!(name in df)){
         df[name] = value;
     }
 }
 
-// Default JS Definitions
+/* Default JS Definitions */
 setVar("avTls", {
     0: {
             name: "Ҳеҷ",
@@ -51,8 +64,8 @@ setVar("avTls", {
             target: 3
         },
 });
-setVar("allowedChars", 'QWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm:,.\\-1234567890\/ ');
-setVar("allowedTrans", /^([QWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm1234567890]{3,4})\//);
+setVar("allowedChars", 'ёйқукенгшҳзхъӯғэждлорпавҷфячсмитӣбюЁҒӮЪХЗҲШГНЕКУҚЙФҶВАПРОЛДЖЭЮБӢТИМСЧЯQWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm:,.\\-1234567890\/ ');
+setVar("allowedTrans", /^([ёйқукенгшҳзхъӯғэждлорпавҷфячсмитӣбюЁҒӮЪХЗҲШГНЕКУҚЙФҶВАПРОЛДЖЭЮБӢТИМСЧЯQWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm1234567890]{3,4})\//);
 setVar("allowedBook", /([A-Za-z]+)((( ?)([A-Za-z]+))?)/);
 setVar("appName", "KITOB | The Bible");
 setVar("audioProvider","");
@@ -62,7 +75,7 @@ setVar("thiredOf", "3 ");
 setVar("fourthOf", "4 ");
 setVar("gospel", "gospel"); // needed if in bookname, so "matthew" instead of "gospel of ..." is displayed
 setVar("pbBook1", "nothing"); // same
-setVar("defBook", "Matthew");
+setVar("defBook", "470");
 setVar("inexistent", "asdfjkl"); // needed to force search
 setVar("noSearchResult", "This word(s) is (are) not found.");
 setVar("verseNotFound1", "In");
